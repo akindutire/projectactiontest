0cbdeac3f9c872dafa7b9f5b74b1e61914b8b970
akindutire <akinsamuel33@gmail.com>
2020-01-17 02:05:02 +0100

    added all host to cors in actioncontroller

--- src/main/java/com/vggpro/projectaction/controller/ActionController.java
+++ src/main/java/com/vggpro/projectaction/controller/ActionController.java
@@ -3,6 +3,7 @@ package com.vggpro.projectaction.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ActionController {
 
 	@Autowired
